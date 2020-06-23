227c3d3cbf5767591ea8e359ddc752c9edd862eb
ElMonjeDeVerdad <49131470+ElMonjeDeVerdad@users.noreply.github.com>
2020-06-23 04:31:48 +0200

    Add files via upload

--- DIF/urls.py
+++ DIF/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('DIF/', include('apps.dashboard.urls')),
+    path('', include('apps.dashboard.urls')),
 ]
